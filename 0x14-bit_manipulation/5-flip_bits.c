2b76f98e920d888c74984cc2de0c2b2e2013107b
Anthony <bondolo90@gmail.com>
2023-04-07 06:50:00 -0700

    5. 101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * count_different_bits - counts the number of bits that
+ * flip_bits - counts the number of bits that
  * are different between two numbers
  * @n: first number
  * @m: second number
  *
  * Return: the number of different bits between n and m
  */
-unsigned int count_different_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int num_different_bits = 0;
 
