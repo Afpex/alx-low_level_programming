1fd68379b40cabd143018573d187daa404350bb6
Anthony <bondolo90@gmail.com>
2023-04-11 11:21:35 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,9 +31,8 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != '\0'; i++)
 	{
 		if (write(fd, &text_content[b], 1) == -1)
-		{
 			return (-1);
-		}
+	}
 	close(fd);
 	return (1);
 }
