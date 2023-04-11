4a888be651a81e5f24d48cc2e5d0515eb3494327
Anthony <bondolo90@gmail.com>
2023-04-11 10:28:39 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int i;
-	char *buf;
+	int b;
 
 	if (filename == NULL)
 		return (-1);
@@ -29,23 +28,12 @@ int create_file(const char *filename, char *text_content)
 		else
 			return (-1);
 	}
-	buf = malloc(sizeof(char) * (strlen(text_content) + 1));
-	if (buf == NULL)
-	{
-		close(fd);
-		return (-1);
-	}
 	for (i = 0; text_content[i] != '\0'; i++)
-		buf[i] = text_content[i];
-		buf[i] = '\0';
-
-		if (write(fd, buf, strlen(buf)) == -1)
+	{
+		if (write(fd, &text_content[b], 1) == -1)
 		{
-			close(fd);
-			free(buf);
 			return (-1);
 		}
 	close(fd);
-	free(buf);
 	return (1);
 }
