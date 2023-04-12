a9c741ba4f4cce8a8870fd1b201dd7f88b6b5b61
Anthony <bondolo90@gmail.com>
2023-04-12 01:49:17 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * create_file - Function that creates a file.
- * @filename: The name of the file to create.
- * @text_content: Is a string to write to the file.
- * Return: 1 on success, -1 on failure.
+ * create_file - creates a file and writes to it
+ * @filename: the name of the file to create
+ * @text_content: the content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int b;
+	int fd, nletters, wstatus;
 
 	if (filename == NULL)
 		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
 
-	fd = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0600);
-	if (fd < 0)
+	for (nletters = 0; text_content[nletters]; nletters)
+	;
+
+	wstatus = write(fd, text_content, nletters);
+	if (wstatus == -1)
 	{
-		if (errno == EEXIST)
-		{
-			fd = open(filename, O_WRONLY | O_TRUNC);
-			if (fd == -1)
-				return (-1);
-		}
-		else
-			return (-1);
+		close(fd);
+		return (-1);
 	}
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		if (write(fd, &text_content[b], 1) == -1)
-			return (-1);
-	}
+
 	close(fd);
 	return (1);
 }
