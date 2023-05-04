17fe14bb50e85e70a5375c8399059654d74c4650
Afpex <bondolo90@gmail.com>
2023-05-04 04:41:45 -0700

    1.1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,13 +9,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
+
+	while (mask)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		if (n & mask)
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
 	}
-	else
+	if (!flag)
 	{
 		_putchar('0');
 	}
