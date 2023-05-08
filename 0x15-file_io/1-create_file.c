182abc35baf635941eca23fcb814c717228d4c24
Afpex <bondolo90@gmail.com>
2023-05-08 02:30:12 -0700

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - creates a file with the given text content
+ * @filename: pointer to the name of the file to create
+ * @text_content: pointer to the string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0, lenw;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (*(text_content + len))
+			len++;
+		lenw = write(fd, text_content, len);
+		if (lenw != len)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
